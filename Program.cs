33127fcc2fe934e660e143f331b91d38001ce652
Кравченко Дмитро <cravchenko5@ukr.net>
2020-11-23 14:01:02 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstCommit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
